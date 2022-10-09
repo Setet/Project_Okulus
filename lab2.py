114a7f74ac2d2504bdd1037f02c4f08c3f69a6d6
Setet <72457086+Setet@users.noreply.github.com>
2022-10-09 17:26:54 +0300

    Lab2 v1.03

--- lab2.py
+++ lab2.py
@@ -16,33 +16,33 @@ def Lab_2_window():
     def draw():
         fig.clf()
 
-        res_x = txt_3.get()
+        res_x = txt_1.get()
         res_y = txt_2.get()
 
-        beb = fig.add_subplot(projection='3d')
-        beb.plot_surface(x, y, z, rstride=5, cstride=5, alpha=0.5, cmap="inferno")
+        ax = fig.add_subplot(projection='3d')
+        ax.plot_surface(x, y, z, rstride=5, cstride=5, alpha=0.5, cmap="inferno")
         canvas.draw()
 
         x_cs = []
         y_cs = []
         z_cs = []
 
-        for i, point in kp(res_x,res_y):
+        for i, point in kp(res_x, res_y):
             x_cs.append(point[0])
             y_cs.append(point[1])
             z_cs.append(point[2])
 
         for i in range(len(x_cs)):
             if i < (len(x_cs) - 1):
-                beb.scatter(x_cs[i - 1], y_cs[i - 1], z_cs[i - 1], c="black", s=1, marker="s")
+                ax.scatter(x_cs[i - 1], y_cs[i - 1], z_cs[i - 1], c="black", s=1, marker="s")
             else:
-                beb.scatter(x_cs[i - 1], y_cs[i - 1], z_cs[i - 1], c="red")
+                ax.scatter(x_cs[i - 1], y_cs[i - 1], z_cs[i - 1], c="red")
 
             txt.insert(INSERT, f"{i}) ({round(x_cs[i], 2)})({round(y_cs[i], 2)}) = {round(z_cs[i], 4)}\n")
             canvas.draw()
 
             window_lab_2.update()
-            delay = txt_1.get()
+            delay = txt_3.get()
             time.sleep(float(delay))
 
     def delete():
@@ -75,10 +75,15 @@ def Lab_2_window():
     toolbar.update()
     canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-    lbl_1 = Label(left_f, text="Задержка в секундах")
-    lbl_2 = Label(window_lab_2, text="Функция :\n2 * x1^2 + 2 * x1^2 + 2 * x2^2 - 4 * x1 - 6 * x2")
+    lbl_1 = Label(window_lab_2, text="Функция :\n2 * x1^2 + 3 * x2^2 + 4 * x1 * x2 - 6 * x1 - 3 * x2")
+
+    lbl_2 = Label(left_f, text="X")
+    lbl_3 = Label(left_f, text="Y")
+    lbl_4 = Label(left_f, text="Задержка в секундах")
 
     txt_1 = Entry(right_f, width=10)
+    txt_2 = Entry(right_f, width=10)
+    txt_3 = Entry(right_f, width=10)
 
     txt = scrolledtext.ScrolledText(txt_f, width=40, height=10)
 
@@ -92,20 +97,12 @@ def Lab_2_window():
 
     lbl_1.pack(side=TOP, padx=5, pady=5)
     lbl_2.pack(side=TOP, padx=5, pady=5)
-
-    lbl_3 = Label(window_lab_2, text="X")
-    lbl_3.pack(side=LEFT, padx=5, pady=5)
-
-    txt_3 = Entry(window_lab_2, width=10)
-    txt_3.pack(side=LEFT, padx=5, pady=5)
-
-    lbl_2 = Label(window_lab_2, text="Y")
-    lbl_2.pack(side=LEFT, padx=5, pady=5)
-
-    txt_2 = Entry(window_lab_2, width=10)
-    txt_2.pack(side=LEFT, padx=5, pady=5)
+    lbl_3.pack(side=TOP, padx=5, pady=5)
+    lbl_4.pack(side=TOP, padx=5, pady=5)
 
     txt_1.pack(side=TOP, padx=5, pady=5)
+    txt_2.pack(side=TOP, padx=5, pady=5)
+    txt_3.pack(side=TOP, padx=5, pady=5)
 
     txt.pack(padx=5, pady=5)
 
