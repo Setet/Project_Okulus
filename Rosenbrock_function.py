fb5b71d2174be31991890054cceaf64abc847f92
Setet <72457086+Setet@users.noreply.github.com>
2022-10-10 21:05:32 +0300

    Lab2 v1.01

--- Rosenbrock_function.py
+++ Rosenbrock_function.py
@@ -6,7 +6,7 @@ def rosen(x):
     return np.sum(100.0 * (x[1:] - x[:-1] ** 2.0) ** 2.0 + (1 - x[:-1]) ** 2.0, axis=0)
 
 
-def makeData():
+def Make_Data_Lab_3():
     x = numpy.linspace(-2, 2, 100)
     y = numpy.linspace(-1, 3, 100)
 
