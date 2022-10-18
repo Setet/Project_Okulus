d21706b59039b2b39c3d5811471ca4a84503fa90
Artem Yakhontov <72457086+Setet@users.noreply.github.com>
2022-10-18 10:24:33 +0300

    Lab4 v1.03

--- Rosenbrock_function.py
+++ Rosenbrock_function.py
@@ -10,11 +10,11 @@ def rosenbrock_2(x, y):
     return (1.0 - x) ** 2 + 100.0 * (y - x * x) ** 2
 
 
-def Make_Data_Lab_3():
+def make_data_lab_3():
     x = numpy.linspace(-5, 5, 100)
     y = numpy.linspace(-5, 5, 100)
 
-    xgrid, ygrid = numpy.meshgrid(x, y)
+    x_grid, y_grid = numpy.meshgrid(x, y)
 
-    z = rosenbrock(np.array([xgrid, ygrid]))
-    return xgrid, ygrid, z
+    z = rosenbrock(np.array([x_grid, y_grid]))
+    return x_grid, y_grid, z
--- SLSQP.py
+++ SLSQP.py
@@ -7,39 +7,33 @@ def make_data_lab_2():
     x = numpy.linspace(-10, 10, 100)
     y = numpy.linspace(-10, 10, 100)
 
-    xgrid, ygrid = numpy.meshgrid(x, y)
+    x_grid, y_grid = numpy.meshgrid(x, y)
 
-    z = 2 * xgrid * xgrid + 3 * ygrid * ygrid + 4 * xgrid * ygrid - 6 * xgrid - 3 * ygrid
-    return xgrid, ygrid, z
+    z = 2 * x_grid * x_grid + 3 * y_grid * y_grid + 4 * x_grid * y_grid - 6 * x_grid - 3 * y_grid
+    return x_grid, y_grid, z
 
 
 def kp(x, y):
-    global points
+    #   global points
     points = []
 
-    def fun(x):  # Функция
-        x1 = x[0]
-        x2 = x[1]
+    def fun(x_i):  # Функция
+        x1 = x_i[0]
+        x2 = x_i[1]
         return 2 * x1 * x1 + 3 * x2 * x2 + 4 * x1 * x2 - 6 * x1 - 3 * x2
 
-    def constraint(x):
-        x1 = x[0]
-        x2 = x[1]
-        return x1 + 2 * x2 - 2
+    def callback(x_w):
+        g_list = np.ndarray.tolist(x_w)
+        g_list.append(fun(x_w))
+        points.append(g_list)
 
-    def callback(Xi):
-        global points
-        glist = np.ndarray.tolist(Xi)
-        glist.append(fun(Xi))
-        points.append(glist)
-
-    b = (0, float("inf"))  # диапозон поиска
-    bnds = (b, b)
+    b = (0, float("inf"))  # диапазон поиска
+    bounds = (b, b)
     x0 = (x, y)  # начальная точка
     con = {'type': 'eq', 'fun': fun}
 
     # основной вызов
-    res = minimize(fun, x0, method="SLSQP", bounds=bnds,
+    res = minimize(fun, x0, method="SLSQP", bounds=bounds,
                    constraints=con, callback=callback)
 
     glist = np.ndarray.tolist(res.x)
