a847897cf93ae90d9d8bb22e67226ae1dcea70a6
Artem Yakhontov <72457086+Setet@users.noreply.github.com>
2022-11-15 20:03:16 +0300

    lab6_7_d v1.01

--- bacterias.py
+++ bacterias.py
@@ -1,6 +1,5 @@
 import random
 from operator import itemgetter
-from functions import *
 
 
 class Bacteria:
@@ -11,7 +10,8 @@ class Bacteria:
         self.pos_y = float(position_y)
 
         self.pop_numb = int(population)
-        self.agents = [[random.uniform(-self.pos_x, self.pos_x), random.uniform(-self.pos_y, self.pos_y), 0.0, 0.0] for _ in
+        self.agents = [[random.uniform(-self.pos_x, self.pos_x), random.uniform(-self.pos_y, self.pos_y), 0.0, 0.0] for
+                       _ in
                        range(self.pop_numb)]
 
         for i in self.agents:
@@ -21,28 +21,24 @@ class Bacteria:
         self.chemo_step = chemotaxis
         self.licvid = licvidation
 
-
     def chemotaxis(self, coef):
         for bac in self.agents:
-            vec = [coef* random.uniform(-1, 1), coef* random.uniform(-1, 1)]
+            vec = [coef * random.uniform(-1, 1), coef * random.uniform(-1, 1)]
             for _ in range(self.chemo_step):
                 f = bac[2]
 
-                bac[0] += vec[0]                    # X
-                bac[1] += vec[1]                    # Y
+                bac[0] += vec[0]  # X
+                bac[1] += vec[1]  # Y
                 bac[2] = self.func(bac[0], bac[1])  # Z / Фитнес-Функция
-                bac[3] += bac[2]                    # Health 
+                bac[3] += bac[2]  # Health
 
                 if f < bac[2]:
-                    vec = [coef* random.uniform(-1, 1), coef* random.uniform(-1, 1)]
-    
+                    vec = [coef * random.uniform(-1, 1), coef * random.uniform(-1, 1)]
 
     def reproduction(self):
         self.agents = sorted(self.agents, key=itemgetter(3), reverse=False)
-        for i in range(self.pop_numb//2):
-            self.agents[self.pop_numb//2+i] = self.agents[i].copy()
-        
-
+        for i in range(self.pop_numb // 2):
+            self.agents[self.pop_numb // 2 + i] = self.agents[i].copy()
 
     def elimnination(self):
         for bac in self.agents:
@@ -52,13 +48,5 @@ class Bacteria:
                 bac[2] = self.func(bac[0], bac[1])
                 bac[3] = bac[2]
 
-
     def get_best(self):
         return sorted(self.agents, key=itemgetter(2), reverse=False)[0]
-    
-#my_bactries = Bacteria(rosenbrock_2,100, 5 ,0.2,5,5)
-#for i in range(50):
-#    my_bactries.chemotaxis(1/(i+1))
-#    my_bactries.reproduction()
-#    my_bactries.elimnination()
-#    print(my_bactries.get_best())
